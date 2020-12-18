8f6b64dced656e1247744f27492a82c070f2cfe7
mei <3396790998@qq.com>
2020-12-18 16:59:54 +0800

    针对guest的接口测试框架

---
+++ interface/get_event_list_test.py
@@ -0,0 +1,40 @@
+import requests
+import unittest
+
+
+class GetEventListTest(unittest.TestCase):
+    '''查询发布会接口测试'''
+
+    def setUp(self):
+        self.url = 'http://127.0.0.1:8000/api/get_event_list/'
+        self.result = ''
+
+    def tearDown(self):
+        print(self.result)
+
+    def test_get_event_null(self):
+        '''发布会id为空'''
+        r = requests.get(self.url, params={'eid': ''})
+        result = r.json()
+        self.assertEqual(result['status'], 10021)
+        self.assertEqual(result['message'], 'parameter error')
+
+    def test_get_event_error(self):
+        '''发布会id不存在'''
+        r = requests.get(self.url, params={'eid': '901'})
+        result = r.json()
+        self.assertEqual(result['status'], 10022)
+        self.assertEqual(result['message'], 'query result is empty')
+
+    def test_get_event_success(self):
+        '''发布会id为1，查询成功'''
+        r = requests.get(self.url, params={'eid': '1'})
+        result = r.json()
+        self.assertEqual(result['status'], 200)
+        self.assertEqual(result['message'], 'success')
+        self.assertEqual(result['data']['name'], '红米Pro发布会')
+        self.assertEqual(result['data']['address'], '北京会展中心')
+        self.assertEqual(result['data']['start_time'], '2016-08-20T00:25:42')
+
+if __name__ == '__main__':
+    unittest.main()
